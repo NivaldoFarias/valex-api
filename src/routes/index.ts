8bfcae3785838e087251ade1a979f01283969c8b
Nivaldo Farias <badivia.f@gmail.com>
2022-07-11 14:41:15 -0300

    feat: add /payments route to main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import cardRouter from './card.route';
+import paymentRouter from './payment.route';
 
 const router = Router();
 
 router.use('/cards', cardRouter);
+router.use('/payments', paymentRouter);
 
 export default router;
--- src/routes/payment.route.ts
+++ src/routes/payment.route.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import * as controller from '../controllers/payment.controller';
+import * as middleware from '../middlewares/payment.middleware';
 
 import validateSchema from '../middlewares/schema.middleware';
 import paymentSchema from '../models/payment.model';
@@ -11,5 +12,9 @@ const endpoint = '/payments';
 paymentRouter.post(
   '/',
   validateSchema(paymentSchema, `${endpoint}/`),
+  middleware.newPaymentQueries,
+  middleware.newPaymentValidations,
   controller.newPayment,
 );
+
+export default paymentRouter;
